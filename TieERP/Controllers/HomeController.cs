351e48d6ee554b6b64e7560609427982a7e8ad2c
djsb91 <sbaranczuk@gmail.com>
2020-05-23 09:59:24 +0200

    Added search machine

--- TieERP/Controllers/HomeController.cs
+++ TieERP/Controllers/HomeController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TieERP.Models;
 
 namespace TieERP.Controllers
 {
     public class HomeController : BaseController
     {
-        public ActionResult Index()
+        public ActionResult Index(string search = null)
         {
-            var model = _db.Warehouses.ToList();
+            IEnumerable<Warehouse> model;
+            if (!string.IsNullOrEmpty(search))
+                model = _db.Warehouses.Where(c => c.Name.Contains(search));
+            else
+                model = _db.Warehouses;
 
+            var modelPreview = _db.Warehouses.ToList(); // Push model to list
 
             return View(model);
         }
